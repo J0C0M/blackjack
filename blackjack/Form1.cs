a3165d363a71a3c4cfef06b7a94faa2d2dc04028
cavia <caviagamer.2018@gmail.com>
2025-03-25 01:05:51 +0100

    feat: Deck The deck for blackjack is made

--- blackjack/Form1.cs
+++ blackjack/Form1.cs
@@ -2,9 +2,13 @@ namespace blackjack
 {
     public partial class Form1 : Form
     {
+        private Deck deck;
+
         public Form1()
         {
             InitializeComponent();
+            deck = new Deck();
         }
     }
 }
+
